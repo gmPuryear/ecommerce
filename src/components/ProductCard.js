9589bd522330a90964a20701eba24e25389f1041
George Puryear <gm.puryear@yahoo.com>
2022-09-29 16:57:18 -0500

    added a global exported function that formats prices to USD format

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,34 +1,41 @@
 function ProductCard(products) {
     const productInfo = products.products;
-    console.log(productInfo);
+
+    const convertToUSDFormat = (price) => {
+        return (price).toLocaleString(
+            "en-US",
+            {
+                style: "currency",
+                currency: "usd"
+            }
+        )
+    }
 
 
     const productCard = productInfo.map(product => {
         return (
-
-            <div className="flex justify-center">
+            <div key={product.id} className="flex justify-center mt-8">
                 <div className="rounded-lg shadow-lg bg-white max-w-sm">
-                        <img className ="rounded-t-lg" src = {product.image} alt="product picture"/>
+                    <div className="flex justify-center pt-6">
+                        <img className="h-56 rounded-t-lg" src={product.image} alt="product picture"/>
+                    </div>
                     <div className="p-6">
-                        <h5 className="text-gray-900 text-xl font-medium mb-2">{product.title}</h5>
+                        <h5 className="justify-center text-gray-900 text-xl font-medium mb-2">{product.title}</h5>
                         <p className="text-gray-700 text-base mb-4">
                             {product.description}
                         </p>
-                        <p>${product.price}</p>
+                        <p>Price: {convertToUSDFormat(product.price)}</p>
                         <button type="button"
-                                className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button
+                                className="mt-4 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs
+                                 leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg
+                                 focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800
+                                 active:shadow-lg transition duration-150 ease-in-out">
+                            Add To Cart
                         </button>
                     </div>
                 </div>
-            </div>)
-
-            // <div className="border-2 border-black" key={product.id}>
-            //     <img src={product.image} alt="product picture"/>
-            //     <div>{product.title}</div>
-            //     <div>{product.description}</div>
-            //     <div>{product.price}</div>
-            // </div>)
-
+            </div>
+        )
     })
 
     return (<div>
